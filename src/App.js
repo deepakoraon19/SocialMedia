8e00f84530f3ee940259eabb213840786ae6102b
Devansh707 <devanshsrivastava707@gmail.com>
2023-11-26 18:31:21 +0530

    Adding Few dependencies and Layout and creating pages and components.
    Adding Few dependencies and Layout and creating pages and components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import { Outlet, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Home, Login, Profile, Register, ResetPassword } from './pages';
 
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  // When the user log-in, the user will get a token
+  return user?.token ? (
+    <Outlet/> // if the user has the token, then it can access all the pages in the outlet
+  ) :  (
+    // Else navigate the user to login page and also pass the state of location from where it was accessing
+    <Navigate to='/login' state= {{ from : location}} replace />
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Socia
-        </p>
-      </header>
+    <div >
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile:id' element={<Profile />} />
+        </Route>
+
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import ResetPassword from './ResetPassword';
+import Login from './Login';
+import Profile from './Profile';
+import Register from './Register';
+
+export {Home, ResetPassword, Login, Profile, Register};
