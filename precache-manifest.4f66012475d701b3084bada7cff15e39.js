ace6880d4cb6da41d49a277d7301702bde875a9e
deepakoraon19 <deepak.19ranchi@gmail.com>
2023-11-27 12:36:17 +0530

    Updates

---
+++ precache-manifest.4f66012475d701b3084bada7cff15e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b0d46cb6424f448d193bb59a097da8",
+    "url": "/SocialMediaFrontend/index.html"
+  },
+  {
+    "revision": "b00b3b38e2a75ba66cf7",
+    "url": "/SocialMediaFrontend/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "c96722d8d80f1d64c483",
+    "url": "/SocialMediaFrontend/static/js/2.53f85abf.chunk.js"
+  },
+  {
+    "revision": "653ac1c43bf30a302d9476ac1d639668",
+    "url": "/SocialMediaFrontend/static/js/2.53f85abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4f489166ca4505d40d",
+    "url": "/SocialMediaFrontend/static/js/3.fb0d8701.chunk.js"
+  },
+  {
+    "revision": "b00b3b38e2a75ba66cf7",
+    "url": "/SocialMediaFrontend/static/js/main.39d8f07f.chunk.js"
+  },
+  {
+    "revision": "ffa4adcee3746c289018",
+    "url": "/SocialMediaFrontend/static/js/runtime-main.1854baf5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/SocialMediaFrontend/static/media/logo.06e73328.svg"
+  }
+]);
