bfc0adf49d8c05e65ad17b6d9f075420d20f460c
deepakoraon19 <deepak.19ranchi@gmail.com>
2023-12-06 10:03:24 +0530

    basic api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
 import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Home, Login, Profile, Register, ResetPassword } from "./pages";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Layout() {
-  const user = { name: "test", userId : "userId", token: "test" };
+  const [user, setUser] = useState({});
   const location = useLocation();
-
+  useEffect(p =>{
+    // fetch("http://127.0.0.1:8080/user", {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "Access-Control-Allow-Origin": "*"
+    //   },
+    // }).then(j => console.log(j))
+    axios.get(`http://127.0.0.1:8080/user`).then(j => console.log(j))
+  }, [])
   // When the user log-in, the user will get a token
   return user?.token ? (
     <Outlet user={user}/> // if the user has the token, then it can access all the pages in the outlet
